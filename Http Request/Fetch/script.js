5465159df17abf051195a7fce4fde086aa50f029
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-10-29 23:58:38 -0400

    first fetch api ever complete

---
+++ Http Request/Fetch/script.js
@@ -0,0 +1,10 @@
+fetch("https://swapi.dev/api")
+        .then( res => console.log(res))
+
+
+
+
+
+//         fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
