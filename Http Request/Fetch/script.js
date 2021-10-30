413e18c709a44b6870948b60cf77c50859aea7d3
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-10-30 02:16:22 -0400

    have fun with fetch property

--- Http Request/Fetch/script.js
+++ Http Request/Fetch/script.js
@@ -1,10 +1,51 @@
-fetch("https://swapi.dev/api")
-        .then( res => console.log(res))
+const url = "https://swapi.dev/api";
+
+fetch(url)
+        .then( res => {
+            res.json()
+            .then( data => {
+                const filmUrl = data.films;
+                const peopleUrl = data.people;
+                const planetsUrl = data.planets;
+                const speciesUrl = data.species;
+                const starshipsUrl = data.starships;
+                const vehiclesUrl = data.vehicles;
+                console.log(filmUrl, peopleUrl, planetsUrl, speciesUrl, starshipsUrl, vehiclesUrl)
+                console.log(data)
+                
+                //printing the films
+                fetch(filmUrl)
+                            .then(res => {
+                                res.json()
+                                        .then( films => { 
+                                            for (let film of films.results) {
+                                                console.log(film.title)
+                                            }
+                                            console.log("\n")
+                                        })
+                            })
+
+                
+                //printing the planets
+               fetch(peopleUrl)
+                            .then(res => {
+                                res.json()
+                                        .then( people => {
+                                            console.log(people.results)
+                                            for (let person of people.results) {
+                                                console.log(person.name)
+                                            }
+                                        })
+                            })
+                
+            })
+
+        })
 
 
-
-
-
-//         fetch('http://example.com/movies.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+films: "https://swapi.dev/api/films/"
+people: "https://swapi.dev/api/people/"
+planets: "https://swapi.dev/api/planets/"
+species: "https://swapi.dev/api/species/"
+starships: "https://swapi.dev/api/starships/"
+vehicles: "https://swapi.dev/api/vehicles/"
