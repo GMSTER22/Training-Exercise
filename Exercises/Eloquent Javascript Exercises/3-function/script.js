46d179508eee2dd3105e94ab219542482c3dcee4
Gael Souleymane Nongnogo <gmster22@gmail.com>
2021-12-16 19:11:04 -0500

    train on exercise 6 from ZTM and eloquent javascript

---
+++ Exercises/Eloquent Javascript Exercises/3-function/script.js
@@ -0,0 +1,13 @@
+// Write a Math.min function
+
+const minimumSecond = (a, b) => {
+   return a - b < 0 ? a : b 
+}
+
+console.log(minimumSecond(3,2))
+
+// Exercise on recursion
+
+function isEven(input) {
+    
+}
