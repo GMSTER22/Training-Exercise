1e86a6c2164995a0f04357ac75c1ecbba6021a33
Gael Souleymane Nongnogo <gmster22@gmail.com>
2022-03-19 23:50:04 -0400

    learning how to build a basic library for greetings called greeter

---
+++ Building a Framework/Greeter.js
@@ -0,0 +1,93 @@
+(function(global, $) {
+    const Greeter = (firstName, lastName, language) => {
+        return new Greeter.init(firstName, lastName, language);
+    }
+
+    const supportedLangs = ["en", "es"];
+
+    var greetings = {
+        en: "Hello",
+        es: "Hola"
+    }
+
+    var formalGreetings = {
+        en: "Greetings",
+        es: "Saludos"
+    }
+
+    var logMessages = {
+        en: "Logged in",
+        es: "Inicio sesion"
+    }
+
+    Greeter.prototype = {
+        fullName: function() {
+            return this.firstName + " " + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + " " + this.firstName + "!"
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + " " + this.fullName() + "!"
+        },
+
+        greet: function(formal) {
+            let msg;
+
+            //if undefined or null it will be coerced to "false"
+            if (formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting()
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            //"this" refers to the calling object at execution time
+            //makes the method chainable
+
+            return this;
+        },
+
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ": " + this.fullName());
+            }
+
+            return this;
+        },
+
+        setLand: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+
+    }
+
+    Greeter.init = function(firstName, lastName, language) {
+        
+        let self = this;
+        self.firstName = firstName || "";
+        self.lastName = lastName || "";
+        self.language = language || "en";
+
+    }
+
+    Greeter.init.prototype = Greeter.prototype;
+
+    global.Greeter = global.G$ = Greeter;
+
+}(window, jQuery))
